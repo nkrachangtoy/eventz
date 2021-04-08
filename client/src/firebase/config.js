b2866ed15e613df719dce5de53846899c7c8c18a
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-04-08 14:08:21 -0700

    added firebase config

---
+++ client/src/firebase/config.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase"
+import "@firebase/auth"
+import "@firebase/firestore"
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCVdm4ILruwejiSkdiD7bpdX14Yb8Br8is",
+    authDomain: "eventz-website.firebaseapp.com",
+    projectId: "eventz-website",
+    storageBucket: "eventz-website.appspot.com",
+    messagingSenderId: "1011473878677",
+    appId: "1:1011473878677:web:0c04138df5bbc7924f959c"
+  };
