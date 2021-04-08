d51456ab2304e3e19150da0a1acb628cf1f3e663
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-04-08 16:07:31 -0700

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,16 @@
 import React, { useState, useEffect, useContext } from "react"
-import  firebase  from "./firebase/config"
+import Register from "./components/Register"
+// import  firebase  from "./firebase/config"
 
 
 export default function App() {
 const [user, setUser] = useState(null)
-const [email, setEmail] = useState("email")
-const [password, setPassword] = useState("password")
 
-const createUser = async (email, password) => {
 
-  try {
-    const data = await firebase.auth().createUserWithEmailAndPassword(email, password)
-    console.log(data.user.uid)
-    console.log('USER CREATED')
-  } catch (error) {
-    console.log(error)
-    console.log("ERROR OCCURED")
+
+
+  return (
+    <Register></Register>
+  )
   }
-
-}
-
-return (
-
-<div>
-
- <label>
-    email:
-    <input type="text" name="name" value={email} onChange={(e)=> setEmail(e.target.value)}/>
-  </label>
-  <label>
-    email:
-    <input type="text" name="password" value={password} onChange={(e)=> setPassword(e.target.value)}/>
-  </label>
-<button onClick={() => createUser(email, password)}>Click me</button> 
-</div>
-)
-
-
-}
 
