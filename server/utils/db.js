51a77aa055886dd40b3a980a3e9bd80168db3746
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-04-09 01:05:46 -0700

    install dependencies and connect to mongodb #12

---
+++ server/utils/db.js
@@ -0,0 +1,18 @@
+// require
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+// Mongodb URI link
+const dbURI = process.env.URI
+
+module.exports.connectDB = async () => {
+    await mongoose.connect(dbURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(res => console.log('Connected to Mongodb'))
+    .catch(err => console.log(err))
+}
+
