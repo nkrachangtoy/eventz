c493e4934743f0a87a0a44ce32703b8267401d70
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-04-09 17:15:26 -0700

    Added files for basic pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect, useContext } from "react"
 import Header from "./components/Header"
 import Login from "./components/Login"
 import Register from "./components/Register"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 // import  firebase  from "./firebase/config"
 
 
@@ -10,11 +16,31 @@ const [user, setUser] = useState(null)
 
 
   return (
-    <>
-    <Header />
-    <Login></Login>
-    <Register></Register>
-    </>
+  <Router>
+    <HeaderNavigation></HeaderNavigation>
+    <main style={{marginTop: 100}}>
+    <Switch>
+      <Route path="/login">
+        <Login></Login>
+      </Route>
+      <Route path="/register">
+        <Register></Register>
+      </Route>
+      {/* <Route path="/profile">
+        <p>Profile Page</p>
+      </Route>
+      <Route path="/newPost">
+        <p>Create New Post Page</p>
+      </Route>
+      <Route path="/posts/:postId">
+        <PostDetailsPage></PostDetailsPage>
+      </Route> */}
+      <Route path="/">
+        
+      </Route>
+    </Switch>
+    </main>
+  </Router>
   )
   }
 
