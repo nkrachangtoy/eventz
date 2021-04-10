a74b7ec78897ed1060e2301e09b695ed5d999960
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-04-10 10:40:43 -0700

    working on implementing material ui data grid

--- client/src/components/Events/index.js
+++ client/src/components/Events/index.js
@@ -1,12 +1,22 @@
 import React, { useState, useEffect, useContext } from "react"
 import  firebase  from "../../firebase/config"
+import { DataGrid, GridRowsProp, GridColDef } from '@material-ui/data-grid';
 
 
 export default function Events() {
     
     return (
-        <div>
-            <h1>Events Page</h1>
-        </div>
+    <div style={{ height: 300, width: '100%' }}>
+        <DataGrid
+          columns={[{ field: 'Events Name' }, { field: 'Detail' }, { field: 'age' }]}
+          rows={[
+            {
+              id: 1,
+              username: 'defunkt',
+              age: 38,
+            },
+          ]}
+        />
+    </div>
     )
 }
