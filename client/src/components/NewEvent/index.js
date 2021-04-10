a59ed8cca0e00a5230d0589b61a879b5014a6e75
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-04-09 18:05:14 -0700

    pulled from main

---
+++ client/src/components/NewEvent/index.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useContext } from "react"
+import  firebase  from "../../firebase/config"
+
+
+export default function NewEvent() {
+    
+    return (
+        <div>
+            <h1>New Events Page</h1>
+        </div>  
+    )
+}
