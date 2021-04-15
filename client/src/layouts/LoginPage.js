0850d7251f855113242e71adc427b6e70bb3b70f
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-04-15 00:25:37 -0700

    create login and register page layout

---
+++ client/src/layouts/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginForm from '../components/Login/index'
+
+function LoginPage() {
+    return (
+        <div className="container">
+            <LoginForm></LoginForm>
+        </div>
+    )
+}
+
+export default LoginPage
