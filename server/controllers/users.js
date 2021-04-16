c8d7958f78bc9cf53f019940c7f785e1b6ddb2eb
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-04-15 22:41:08 -0700

    create user controller and edit user schema

---
+++ server/controllers/users.js
@@ -0,0 +1,22 @@
+// // Require
+// const User = require('../models/user')
+// const FirebaseToken = require('../utils/firebaseToken')
+
+// /**
+//  * REGISTER NEW USER
+//  * @METHOD POST
+//  */
+// module.exports.registerUser = async (req,res) => {
+//     const {_id, email, firstName, lastName, password} = req.body
+
+//     const user = new User({
+//         email,
+//         firstName,
+//         lastName,
+//     })
+//     try {
+    
+//     } catch (err) {
+        
+//     }
+// }
