9e8bfbe584983efbabc55810d816a9aff5b5e5a9
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-04-16 12:24:56 -0700

    firebase token now works

--- client/src/network/users.js
+++ client/src/network/users.js
@@ -1,18 +1,18 @@
 import axios from 'axios'
-// import firebase from '../firebase/config'
+import firebase from '../firebase/config'
 
-// const createToken = async () => {
-//     const user = firebase.auth().currentUser
-//     const token = user && (await user.getIdToken())
+const createToken = async () => {
+    const user = firebase.auth().currentUser
+    const token = user && (await user.getIdToken())
 
-//     const payloadHeader = {
-//         headers: {
-//             'Content-Type': 'application/json',
-//             Authorization: `Bearer ${token}`
-//         },
-//     }
-//     return payloadHeader
-// }
+    const payloadHeader = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+    }
+    return payloadHeader
+}
 
 const URL = 'http://localhost:8080/users'
 
@@ -21,7 +21,7 @@ const URL = 'http://localhost:8080/users'
  */
 
 export const registerUser = async (user) => {
-    // const header = await createToken()
+    const header = await createToken()
     const payload = {
         email: user.email,
         firstName: user.firstName,
@@ -29,7 +29,7 @@ export const registerUser = async (user) => {
         password: user.password,
     }
     try{
-    const response = await axios.post(URL, payload)
+    const response = await axios.post(URL, payload, header)
     return response.data
     }catch(err){
         console.error(err)
