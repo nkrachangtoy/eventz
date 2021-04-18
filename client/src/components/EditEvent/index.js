99c284aca7f50539130dbd1bed1713df1a896f07
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-04-17 21:34:49 -0700

    added basic styling and variables

--- client/src/components/EditEvent/index.js
+++ client/src/components/EditEvent/index.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from "react"
+import { Link } from 'react-router-dom'
+import { createEvent } from '../../network/events'
+import { makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom'
+import { TextField } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
 
 export default function EditEvent() {
+    const classes = useStyles();
+    const history = useHistory()
+    const [title, setTitle] = useState()
+    const [description, setDescription] = useState()
+    const [location, setLocation] = useState()
+    const [time, setTime] = useState()
+
+    const handleEditEvent = (e) => {
+        e.preventDefault()
+        createEvent(title,description,location,time)
+        setTitle('')
+        setDescription('')
+        setLocation('')
+        setTime('')
+        history.push('/events')
+    }
+
     return (
         <div>
-            <form onSubmit={handleCreateEvent} className={classes.root} noValidate autoComplete="off">
+            <form onSubmit={handleEditEvent} className={classes.root} noValidate autoComplete="off">
                 <div>
                     <TextField
                     id="outlined-multiline-flexible"
